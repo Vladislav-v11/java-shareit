c6d35b2d14055eb395f1ce60f0d79151c6154ddb
Vladislav <vladislaw.sirotkin@yandex.ru>
2026-08-04 18:41:36 +0300

    Модель, DTO

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingResponse.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit.booking.dto;
+
+
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.booking.model.BookingStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BookingResponse {
+    private long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private ItemInfo item;
+    private BookerInfo booker;
+    private BookingStatus status;
+
+    @Data
+    @Builder
+    public static class ItemInfo {
+        private long id;
+        private String name;
+    }
+
+    @Data
+    @Builder
+    public static class BookerInfo {
+        private long id;
+        private String name;
+    }
+}
